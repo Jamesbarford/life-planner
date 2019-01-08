0bb8a5f2cade5f0a2525d0128974f1aab515f6d7
James Barford-Evans <jamesbarfordevans@gmail.com>
2019-01-08 22:41:11 +0000

    docs for helper class

--- src/Dates/dateHelper.ts
+++ src/Dates/dateHelper.ts
@@ -37,12 +37,6 @@ export enum TimePoint {
   millisecond = "millisecond"
 }
 
-
-type TimePointObject = {
-  date: unitOfTime.All,
-  value: number;
-};
-
 export class DateHelper {
 
   /**
@@ -59,7 +53,7 @@ export class DateHelper {
   };
 
   // TIME
-  // 
+  // ===========================================================================
   /**
    * 
    * @param {showSeconds} boolean as to whether to show seconds on clock
@@ -85,25 +79,64 @@ export class DateHelper {
 
   // DAYS
   // ===========================================================================
+  /**
+   * 
+   * @returns {*} day of week as string.
+   * @example
+   * DateHelper.getTodayString();
+   * => "Monday"
+   */
   static getTodayString(): string {
     const today = moment().day();
     return dayNames[today];
   }
 
+  /**
+   * 
+   * @returns {*} day of week as string.
+   * @example
+   * DateHelper.getTodayNumeric();
+   * => 2
+   */
   static getTodayNumeric(): number {
     return moment().day();
   }
 
+
+  /**
+   * 
+   * @param {day} number 0 - 7, day of week 0: Sunday - 7: Saturday
+   * @returns {*} day of week as string.
+   * @example
+   * DateHelper.dayToString(2);
+   * => Tuesday
+   */
   static dayToString(day: number): string {
     return dayNames[day];
   }
 
-  static getDate(day?: number, format?: boolean): number | string | Moment {
+  /**
+   * 
+   * @param {date} 0 - 31
+   * @returns {*} time in milliseconds.
+   * @example
+   * DateHelper.getDate();
+   * => 123425432113
+   */
+  static getDate(day?: number): Moment {
     const today = moment().day();
     return moment().date(day || today);
   }
 
-  static formatDate(date: number) {
+  /**
+   * 
+   * @param {date} 0 - 31
+   * @returns {*} formatted date.
+   * @example
+   * DateHelper.formatDate(31);
+   * => "31st"
+   */
+  static formatDate(date: number): string {
     switch(date) {
       case 1:
       case 21:
@@ -124,6 +157,15 @@ export class DateHelper {
   }
   
 
+   /**
+   * 
+   * @param {month} number 0 - 11
+   * @param {year} number
+   * @returns {*} how many days are in the month of that given year.
+   * @example
+   * DateHelper.getNumberOfDaysInMonth(0, 2019);
+   * => 31
+   */
   static getNumberOfDaysInMonth(month: number, year: number): number {
     const monthForDate = month + 1;
     return new Date(year, monthForDate, 0).getDate();
@@ -131,37 +173,80 @@ export class DateHelper {
 
   // WEEK
   // ===========================================================================
-  static getWeek() {
+  /**
+   * 
+   * @returns {*} week of the year.
+   * @example
+   * DateHelper.getWeek();
+   * => 50
+   */
+  static getWeek(): number {
     return moment().week();
   }
 
   // MONTHS
   // ===========================================================================
+  /**
+   * 
+   * @returns {*} month in numerical form.
+   * @example
+   * DateHelper.getMonthNumeric();
+   * => 0
+   */
   static getMonthNumeric(): number {
     return moment().month();
   }
 
+  /**
+   * 
+   * @param {month} - number
+   * @returns {*} name of the month as a string.
+   * @example
+   * DateHelper.getMonthString(0);
+   * => "Janurary"
+   */
   static getMonthString(month: number): string {
     return monthNames[month || moment().month()];
   }
 
   // YEAR
   // ===========================================================================
+  /**
+   * 
+   * @returns {*} current year.
+   * @example
+   * DateHelper.getYear();
+   * => 2019
+   */
   static getYear(): number {
     return moment().year();
   }
 
   // SET DATE
   // ===========================================================================
-  static setIndividualTimePoint(timePoint: unitOfTime.All, value: number) { 
+  /**
+   * NEEDS TESTING
+   * @param {timePoint} TimePoint - e.g "day"
+   * @param {value} - number for the time point
+   * @returns {*} ?.
+   * @example
+   * DateHelper.setTimePoint(timePoint, value);
+   * => ?
+   */
+  static setTimePoint(timePoint: TimePoint, value: number): Moment {
     return moment().set(timePoint, value);
   }
 
-  static setTimePoint(timePoint: TimePoint, value: number) {
-    return moment().set(timePoint, value);
-  }
-
-  static addTime(timepoint: object) {
-    moment().add(...timepoint as any);
+  /**
+   * NEEDS TESTING
+   * @param {timepoint} - TimePoint
+   * @param {value} - number
+   * @returns {*} ?
+   * @example
+   * DateHelper.addTime()
+   * => ?
+   */
+  static addTime(timepoint: TimePoint, value: number): Moment {
+    return moment().add(value as any, timepoint);
   }
 }
