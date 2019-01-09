fe3ad795babf17267f4fb34dccba8d585faf7d4a
James Barford-Evans <jamesbarfordevans@gmail.com>
2019-01-09 23:49:43 +0000

    added tslint file

--- src/Calendar/index.tsx
+++ src/Calendar/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as moment from 'moment';
-import { Moment } from "moment";
+import { Moment } from 'moment';
 import { cloneDeep } from "lodash";
 
 // HELPERS
