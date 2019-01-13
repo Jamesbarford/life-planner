26ab85fee8ca80fe832b31dc1bad5d6c849aa0de
James Barford-Evans <jamesbarfordevans@gmail.com>
2019-01-13 23:01:31 +0000

    changed import to correct name

--- src/events/actions.ts
+++ src/events/actions.ts
@@ -1,6 +1,6 @@
 import { ActionBase } from "../types/global";
 import { Event } from "./types";
-import { createEvent } from "./factories";
+import { createHashEvent } from "./factories";
 
 export enum EventActions {
   CreateEvent = "Events.CreateEvent"
@@ -9,7 +9,7 @@ export enum EventActions {
 export class CreateEventAction implements ActionBase {
   readonly type = EventActions.CreateEvent;
   constructor(public event: Event) {}
-  public createEvent = () => createEvent(this.event);
+  public createEvent = () => createHashEvent(this.event);
 }
 
 export type EventActionTypes = CreateEventAction;
