2bddf0c3cf5847cd0c99537d1c05393bcaf15784
James Barford-Evans <jamesbarfordevans@gmail.com>
2019-01-13 23:04:07 +0000

    converted hash event creator to one liner

--- src/events/factories.ts
+++ src/events/factories.ts
@@ -2,8 +2,6 @@ import * as uuid from "uuid/v4";
 import { Map } from "immutable";
 import { EventBase } from "./types";
 
-export function createHashEvent<T extends EventBase>(event: T): Map<string, T> {
-  const hash = uuid();
-  const eventMap = Map({ [hash]: event });
-  return eventMap;
-}
+export const createHashEvent = <T extends EventBase>(
+  event: T
+): Map<string, T> => Map({ [uuid()]: event });
