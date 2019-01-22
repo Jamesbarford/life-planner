f551027b1aa0c19e9f87e6e0caaaa620ab392552
James Barford-Evans <jamesbarfordevans@gmail.com>
2019-01-22 21:23:06 +0000

    git config 2

--- aphrodite/jest.config.js
+++ aphrodite/jest.config.js
@@ -3,6 +3,19 @@ module.exports = {
   transform: {
     "^.+\\.(tsx|ts)?$": "ts-jest"
   },
+  globals: {
+    "ts-jest": {
+      tsConfig: "<rootDir>/tsconfig.test.json",
+      // This is so ts-jest doesn't try and run the component in the context of the whole app
+      isolatedModules: true,
+      diagnostics: {
+        // Only run diagnostics on our jest test files
+        pathRegex: ".*\\.jest.test\\.tsx?$",
+        warnOnly: true
+      },
+      window: true
+    }
+  },
   testEnvironment: "node",
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec|unit))\\.(tsx|ts)?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
