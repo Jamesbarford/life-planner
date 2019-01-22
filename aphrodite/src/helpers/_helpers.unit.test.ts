504de47664cad953fca25f75542a37484610858e
James Barford-Evans <jamesbarfordevans@gmail.com>
2019-01-22 23:08:11 +0000

    code formatting

--- aphrodite/src/helpers/_helpers.unit.test.ts
+++ aphrodite/src/helpers/_helpers.unit.test.ts
@@ -12,16 +12,19 @@ describe("Util functions", () => {
   it("should remove whitespace", () => {
     const string = "  h e ll o   ";
     const nowWhiteSpace = removeWhiteSpace(string);
+
     expect(nowWhiteSpace).toEqual("hello");
   });
 
   it("should return true for string", () => {
     const string = "hello";
+
     expect(isString(string)).toBe(true);
   });
 
   it("should return false for a number", () => {
     const number = 2;
+
     expect(isString(number)).toBe(false);
   });
 });
